2be41385de820e9d096e9f80c90cf8c5cb06453e
Slinx92 <danilradostev1209@gmail.com>
2023-06-19 16:04:32 +0300

    perhaps

---
+++ textcheck.js
@@ -0,0 +1,5 @@
+while (true) {
+    if (document.getElementById('edit this if you have small pp').innerHTML.toLowerCase() != "no") {
+        location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    }
+}
