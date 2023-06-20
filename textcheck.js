a32c8c17a283280f699c38f488513880f1842f3a
Slinx92 <danilradostev1209@gmail.com>
2023-06-20 08:27:35 +0300

    i literally give up on this

--- textcheck.js
+++
@@ -1,5 +0,0 @@
-while (true) {
-    if (document.getElementById('edit this if you have small pp').innerHTML.toLowerCase() != "no") {
-        location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    }
-}
