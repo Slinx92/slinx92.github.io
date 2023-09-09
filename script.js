8e2c937f2096791976876051ede0f7ea82176cc7
Slinx92 <danilradostev1209@gmail.com>
2023-09-09 18:04:36 +0300

    i give up :skull:

--- script.js
+++
@@ -1,9 +0,0 @@
-function uploadfile() {
-    let data = document.getElementById("file").files[0];
-    let entry = document.getElementById("file").files[0];
-    fetch('./videos/' + encodeURIComponent(entry.name), {method:'POST',body:data});
-    alert(`Your file has been uploaded!`);
-    console.log(keys(data))
-    console.log(values(data))
-    location.reload();
-};
